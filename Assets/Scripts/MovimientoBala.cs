23024255e8b74859c74e7d564df9a386c6786570
AndresMontoro <andres.montorovenegas@alum.uca.es>
2024-03-15 19:41:32 +0100

    First commit

---
+++ Assets/Scripts/MovimientoBala.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoBala : MonoBehaviour
+{
+    public float velocidad = 5.0f;
+    public float valorHerida = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movDistancia = velocidad * Time.deltaTime;
+        transform.Translate(Vector3.forward * movDistancia);
+    }
+}
